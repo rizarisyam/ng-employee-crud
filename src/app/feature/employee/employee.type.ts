34af007dd96580fa3f0ce165c252a5f31cccc844
Riza risyam <riza.risyam@dealpos.com>
2025-04-22 15:52:23 +0700

    Refactor EmployeeType and update list-employee component for pagination
    - Modified EmployeeType interface to include pagination properties and a data array for Employee objects.
    - Updated list-employee.component.html to bind the table to the new data structure and added pagination support.
    - Enhanced list-employee.component.ts to manage pagination state and fetch employees based on current page and per-page settings.
    - Introduced new imports for input fields and icon fields to support the updated UI.

--- src/app/feature/employee/employee.type.ts
+++ src/app/feature/employee/employee.type.ts
@@ -1,11 +1,21 @@
 export interface EmployeeType {
-    id: string
-    first_name: string
-    last_name: string
-    username: string
-    email: string
-    status: boolean
-    birthdate: string
-    group: string
-    description: string
-  }
+  first: number;
+  prev: any;
+  next: number;
+  last: number;
+  pages: number;
+  items: number;
+  data: Employee[];
+}
+
+export interface Employee {
+  id: string;
+  first_name: string;
+  last_name: string;
+  username: string;
+  email: string;
+  status: boolean;
+  birthdate: string;
+  group: string;
+  description: string;
+}
--- src/app/feature/employee/pages/list-employee/list-employee.component.ts
+++ src/app/feature/employee/pages/list-employee/list-employee.component.ts
@@ -1,14 +1,17 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, effect, inject, OnInit, signal } from '@angular/core';
 import { HttpService } from '../../../../core/services/common-http.service';
 
 import { tap } from 'rxjs';
 import { EmployeeType } from '../../employee.type';
 import { CommonModule } from '@angular/common';
-import { TableModule } from 'primeng/table';
+import { TableModule, TablePageEvent } from 'primeng/table';
 import { ToggleSwitchModule } from 'primeng/toggleswitch';
 import { FormsModule } from '@angular/forms';
 import { TextareaModule } from 'primeng/textarea';
 import { CardModule } from 'primeng/card';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputIconModule } from 'primeng/inputicon';
 @Component({
   selector: 'app-list-employee',
   imports: [
@@ -18,6 +21,9 @@ import { CardModule } from 'primeng/card';
     FormsModule,
     TextareaModule,
     CardModule,
+    IconFieldModule,
+    InputTextModule,
+    InputIconModule,
   ],
   templateUrl: './list-employee.component.html',
   styleUrl: './list-employee.component.scss',
@@ -25,13 +31,35 @@ import { CardModule } from 'primeng/card';
 export class ListEmployeeComponent implements OnInit {
   private httpService = inject(HttpService);
 
-  employees = signal<EmployeeType[]>([]);
+  pageData = signal<{ per_page: number; page: number; query: string }>({
+    per_page: 10,
+    page: 1,
+    query: '',
+  });
 
-  ngOnInit(): void {
+  employees = signal<EmployeeType>(null!);
+
+  constructor() {
+    effect(() => {
+      if (this.pageData()) {
+        this.fetchEmployees();
+      }
+    });
+  }
+
+  loadEmployeesLazy(event: TablePageEvent) {
+    this.pageData.set({
+      per_page: event.rows,
+      page: event.first / event.rows + 1,
+    });
+  }
+
+  ngOnInit(): void {}
+  fetchEmployees() {
     this.httpService
-      .get<EmployeeType[]>('http://localhost:3000/employees', {
-        _limit: 10,
-        _page: 1,
+      .get<EmployeeType>('http://localhost:3000/employees', {
+        _per_page: this.pageData().per_page,
+        _page: this.pageData().page,
       })
       .pipe(tap((res) => this.employees.set(res)))
       .subscribe();
